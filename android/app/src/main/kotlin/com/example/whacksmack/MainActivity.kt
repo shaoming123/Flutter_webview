1d7bee17508f69aebcda103e1760110cad8faeb7
shaoming123 <greatfoodprefer@gmail.com>
2021-11-09 16:58:51 +0800

    First commit

---
+++ android/app/src/main/kotlin/com/example/whacksmack/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.whacksmack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:whacksmack/webview_container.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color _containerColor = Colors.yellow;
+  void changeColor() {
+    setState(() {
+      if (_containerColor == Colors.yellow) {
+        _containerColor = Colors.red;
+        return;
+      }
+      _containerColor = Colors.yellow;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WebViewContainer(
+          'https://flutter-webview.vercel.app/', 'Assignment 1'),
+    );
+  }
+}
